158b7b7ba24b396d0318e64e212ae6eb3b093a88
tema <tema@vf8.ru>
2014-05-21 14:08:27 +0400

    queryString now can have not unique keys. array of objects.

--- 3/3.js
+++ 3/3.js
@@ -1,36 +1,66 @@
 var f1, f2;
-var count = 0;
+var count = 0; // to count form1 and form2
 $('.form2').hide(); // init
 
-function doAjaxPost (formnum) {
-	var fn = '.form' + formnum;
+function doAjaxPost(formnum) {
+    var fn = '.form' + formnum;
     var queryString = $(fn).serialize();
     return queryString;
 }
 
-function whatsNew () {
-	console.log(f1.Name + f2.Name);
+function queryStringToArray(queryString) {
+    var params = [], queries, temp, i, l;
+
+    // Split into key/value pairs
+    queries = queryString.split("&");
+
+    // Convert the array of strings into an array of objects
+    for (i = 0, l = queries.length; i < l; i++) {
+        temp = queries[i].split('=');
+        var obj = {};
+        obj[temp[0]] = temp[1];
+        params.push(obj);
+    }
+    return params;
 }
 
-function run (formnum) {
-	var str = doAjaxPost(formnum);
-	var res = parseQueryString(str);
+// We have two arrays of object and compare them
+function whatsNew() {
+//    console.log(f1);
+//    console.log(f2);
 
-	var fn = '.form' + formnum;
-	$(fn).remove();
+    // check numbers of params
+    if (f1.length < f2.length) {
+        var text = 'Form1 has less (' + f1.length + ') params then Form2 (' + f2.length + ').';
+        console.log(text);
+        $('body').append(text);
+    } else if (f1.length > f2.length) {
+        var text = 'Form1 has more (' + f1.length + ') params then Form2 (' + f2.length + ').';
+        console.log(text);
+        $('body').append(text);
+    }
 
-	if (count++ == 0) {
-		f1 = res;
-		$('.info').text('Now fill 2nd form:');
-		$('.form2').show();
-	} else {
-		f2 = res;
-		$('.info').text('Now fill 1st form:');
-	}
+}
 
-	if (count > 1) {
-		$('.info').text('Here what we got:');
-		whatsNew();
-	}
-	console.log(res);
+function run(formnum) {
+    var str = doAjaxPost(formnum);
+    var res = queryStringToArray(str);
+
+    var fn = '.form' + formnum;
+    $(fn).remove();
+
+    if (count++ == 0) {
+        f1 = res;
+        $('.info').text('Now fill 2nd form:');
+        $('.form2').show();
+    } else {
+        f2 = res;
+        $('.info').text('Now fill 1st form:');
+    }
+
+    if (count > 1) {
+        $('.info').text('Here what we got:');
+        whatsNew();
+    }
+    // console.log(res);
 }
