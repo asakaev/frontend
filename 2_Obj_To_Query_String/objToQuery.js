54932f91610717fde11d71e9e7b248bdb49efa9b
tema <tema@vf8.ru>
2014-05-21 19:11:16 +0400

    object to query string convert

---
+++ 2_Obj_To_Query_String/objToQuery.js
@@ -0,0 +1,10 @@
+serialize = function (obj) {
+    var str = [];
+    for (var p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    return str.join("&");
+};
+
+console.log(serialize({id: 5, name: "Nick" })); // id=5&name=Nick
